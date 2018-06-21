504b0048a92c7627508c89b90c49eca0a2d9e598
rparamo <rparamo11@gmail.com>
2018-06-21 17:02:12 -0500

    added random numbers to images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,50 @@
+// This creates the random number at loading of document. 
 $(document).ready (function() {
 
-  var random = Math.floor(Math.random() * 120) + 19;
+  var random = Math.floor(Math.random() * 101) + 19;
 
   console.log(random);
 
   $(".random-number").text(random);
 
  });
+
+// Random numbers assigned to images. 
+$(document).ready (function() {
+
+    var random = Math.floor(Math.random() * 12) +1;
+
+    console.log(random);
+
+    $("#red").text(random);
+})
  
- 
+$(document).ready (function() {
+
+    var random = Math.floor(Math.random() * 12) +1;
+
+    console.log(random);
+
+    $("#green").text(random);
+})
+
+$(document).ready (function() {
+
+    var random = Math.floor(Math.random() * 12) +1;
+
+    console.log(random);
+
+    $("#blue").text(random);
+})
+
+$(document).ready (function() {
+
+    var random = Math.floor(Math.random() * 12) +1;
+
+    console.log(random);
+
+    $("#diamond").text(random);
+})
+
+ var win;
+ var lost;
