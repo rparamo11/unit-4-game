f06d848372361065ea1f935933ed19550183ab21
rparamo <rparamo11@gmail.com>
2018-06-21 23:41:05 -0500

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,8 @@ $(document).ready (function() {
 
  });
 
+ var counter=0
+
 // Random numbers assigned to images. 
 $(document).ready (function() {
 
@@ -46,5 +48,33 @@ $(document).ready (function() {
     $("#diamond").text(random);
 })
 
+// Click event added to images/random numbers.
+$("#red").on("click", function() {
+    console.log($(this));
+})
+
+$("#green").on("click", function() {
+    console.log($(this));
+})
+
+$("#blue").on("click", function() {
+    console.log($(this));
+})
+
+$("#diamond").on("click", function() {
+    console.log($(this));
+});
+
+
+
  var win;
  var lost;
+
+
+
+//  I needed to create a variable to house the random numbers being clicked on. 
+//  Then, use those numbers and create a function to add the numbers together that were cliked on. 
+//  Then create an operator that would compare the numbers added to the Random number generated.
+//  Create if/else statement in which the added numbers get compared and if they equal to the random generated number 
+//     then add a win else add a lose.
+// Once a win or lose are completed, reset the game and start all over.
